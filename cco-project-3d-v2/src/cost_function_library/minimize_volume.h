9b59c51409a81e502e6dd47a6450ab82108c3a74
bergolho <berg@ice.ufjf.br>
2020-10-23 12:25:44 -0300

    Refactoring CCO-Project - Package 1

---
+++ cco-project-3d-v2/src/cost_function_library/minimize_volume.h
@@ -0,0 +1,31 @@
+#ifndef MINIMIZE_VOLUME_LIB_H
+#define MINIMIZE_VOLUME_LIB_H
+
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+#include "../options/cost_function_config.h"
+#include "../options/local_optimization_config.h"
+#include "../cco/cco.h"
+
+// Cost functions implementations
+extern "C" struct segment_node* minimize_tree_volume_default (struct cco_network *the_network,\
+                                        struct cost_function_config *config,\
+                                        struct local_optimization_config *local_opt_config,\
+                                        const double new_pos[],\
+                                        const std::vector<struct segment_node*> feasible_segments,\
+                                        const std::vector<struct face> obstacle_faces,\
+                                        const std::vector<struct pmj_lat> lat_points);
+
+extern "C" struct segment_node* minimize_tree_volume_with_angle_restriction (struct cco_network *the_network,\
+                                        struct cost_function_config *config,\
+                                        struct local_optimization_config *local_opt_config,\
+                                        const double new_pos[],\
+                                        const std::vector<struct segment_node*> feasible_segments,\
+                                        const std::vector<struct face> obstacle_faces,\
+                                        const std::vector<struct pmj_lat> lat_points);
+
+// Auxiliary functions
+
+#endif
